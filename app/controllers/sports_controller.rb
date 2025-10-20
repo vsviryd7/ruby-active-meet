25fc575c65ab3b974c042479d70dbcf2c97f92d4
vsviryd7 <ol.sviridenko777@gmail.com>
2025-10-20 18:06:52 -0400

    works with option adit, destroy for sports and events

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,15 +1,47 @@
 class SportsController < ApplicationController
+  before_action :set_sport, only: [:show, :edit, :update, :destroy]
+
   def index
-    @sports = Sport.all
+    @sports = Sport.order(:name)
   end
 
-  def show
-    @sport = Sport.find(params[:id])
+  def show; end
+  def new  ; @sport = Sport.new end
+
+  def create
+    @sport = Sport.new(sport_params)
+    if @sport.save
+      redirect_to @sport, notice: "Sport created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def new
+  def edit; end
+
+  def update
+    if @sport.update(sport_params)
+      redirect_to @sport, notice: "Sport updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
-  def edit
+  def destroy
+  if @sport.destroy
+    redirect_to sports_path, notice: "Sport deleted."
+  else
+    redirect_to @sport, alert: @sport.errors.full_messages.to_sentence
+  end
+end
+
+  private
+
+  def set_sport
+    @sport = Sport.find(params[:id])   # uses /sports/:id
+  end
+
+  def sport_params
+    params.require(:sport).permit(:name)
   end
 end
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,6 +1,6 @@
 class Sport < ApplicationRecord
   # Many users can pick this sport as their favorite
-  has_many :users, foreign_key: "favorite_sport_id", dependent: :nullify
+  has_many :users, foreign_key: "favorite_sport_id", dependent: :restrict_with_error
   # Many events can be created for this sport
   has_many :events, dependent: :destroy
 end
